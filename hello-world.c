417644b0af873c75ea68c23471f8d8028d0c0d7e
Tavishi Agarwal <agarwaltavishi1@gmail.com>
2026-09-18 02:33:50 +0530

    Print Hello World

--- hello-world.c
+++ hello-world.c
@@ -1,8 +1,17 @@
-#include <stdio.h>
+/* 
+ * Program: Hello World
+ * Author: Tavishi Agarwal
+ * Purpose: First C program to print a message
+ */
 
-int main(){
+#include <stdio.h>   //Includes standard input-output library
+
+int main()  //Main function: program execution starts here
+{
+  //Print "Hello, World!" to the console 
   printf("Hello, World!\n");
+  //Print "This is my first C program on Github." to the console
   printf("This is my first C program on Github.\n");
 
-  return 0;
+  return 0;  //return 0 indicates program successful execution
 }
